8389ff41a992e99dfde918f3d41fa2a8a248303b
bunnyguy <pipandw@gmail.com>
2025-02-04 14:51:58 -0600

    commits from yesterday, added new launch file

---
+++ launch/imu.launch.py
@@ -0,0 +1,23 @@
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bno055',
+            executable='bno055',
+            name='imu driver',
+            parameters=["~/seals_ws/src/bno055/bno055/params/bno055_params.yaml"]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2'
+        ),
+        launch.actions.IncludeLaunchDescription (
+            launch.launch_description_sources.PythonLaunchDescriptionSource (
+                get_package_share_directory('velodyne') + '/launch/velodyne-all-nodes-VLP16-composed-launch.py'
+            )
+        )
+    ])
